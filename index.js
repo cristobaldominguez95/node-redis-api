c3c69759df5ba5e6af6d762e958fdba2fd735322
Cristóbal Domínguez <cristobaldominguez95@gmail.com>
2018-09-08 20:22:57 +0200

    Create server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(3000, () => console.log('App listening on port 3000!'));
